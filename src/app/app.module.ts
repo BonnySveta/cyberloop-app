9adb302cc9d6d73eaa6224dc4c7eacb99e427398
BonnySveta <bonnysveta@gmail.com>
2021-10-25 18:51:31 +1000

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,65 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import { ChartModule } from 'angular-highcharts';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth'; 
+
+import firebase from 'firebase/compat/app';
+import firebaseui from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+
+var ui = new firebaseui.auth.EmailAuthProvider;
+
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+      
+      forceSameDevice: false,
+      
+      emailLinkSignIn: function() {
+        return {
+          // Additional state showPromo=1234 can be retrieved from URL on
+          // sign-in completion in signInSuccess callback by checking
+          // window.location.href.
+          url: 'https://www.example.com/completeSignIn?showPromo=1234',
+          // Custom FDL domain.
+          dynamicLinkDomain: 'example.page.link',
+          // Always true for email link sign-in.
+          handleCodeInApp: true,
+          // Whether to handle link in iOS app if installed.
+          iOS: {
+            bundleId: 'com.example.ios'
+          },
+          // Whether to handle link in Android app if opened in an Android
+          // device.
+          android: {
+            packageName: 'com.example.android',
+            installApp: true,
+            minimumVersion: '12'
+          }
+        };
+      }
+    }
+  ]
+});
+
+/*
+// Is there an email link sign-in?
+if (ui.isPendingRedirect()) {
+  ui.start('#firebaseui-auth-container', uiConfig);
+}
+// This can also be done via:
+if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+  ui.start('#firebaseui-auth-container', uiConfig);
+}*/
 
 
 @NgModule({
@@ -29,7 +87,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatIconModule,
     MatListModule,
     MatSidenavModule,
-    ChartModule
+    ChartModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'cyberloop-fire',
+    appId: '1:779057090809:web:edc5be92dcf409f2d0d228',
+    databaseURL: 'https://cyberloop-fire-default-rtdb.firebaseio.com',
+    storageBucket: 'cyberloop-fire.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD-o2vcbogWpcEWkS_UiK9bX0oBHvQ2_1o',
+    authDomain: 'cyberloop-fire.firebaseapp.com',
+    messagingSenderId: '779057090809',
+    measurementId: 'G-9WCEX1WMM5',
+  },
   production: true
 };
